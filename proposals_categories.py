a4f4541ba731f0bff4d256c244611fe3d2ef75d6
d3h <diegoeauyonh@gmail.com>
2020-09-01 08:03:57 -0600

    Add topic choices

---
+++ proposals_categories.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from django.utils.translation import ugettext as _
+
+_TOPIC_CHOICES = (('', _(u'Selecciona una categoría')),
+    ('seguridad_alimenticia_y_nutricional', _(u'Seguridad alimenticia y nutricional')),
+    ('infraestructura_economia_y_trabajo', _(u'InfraestructuraEconomia y trabajo')),
+    ('administración_pública', _(u'Administración pública')),
+    ('derechos_sexuales_y_reproductivos', _(u'Derechos sexuales y reproductivos')),
+    ('seguridad_y_justicia', _(u'Seguridad y justicia')),
+    ('transporte_público', _(u'Transporte público')),
+    ('salud', _(u'Salud')),
+    ('medio_ambiente_y_recursos_naturales', _(u'Medio ambiente  y recursos naturales')),
+    ('cultura_y_deportes', _(u'Cultura y deportes')),
+    ('lucha_contra_la_corrupción', _(u'Lucha contra la corrupción')),
+)
