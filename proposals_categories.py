1bf961e32e7989418eac3808fbf9401f5cc5b307
Diego Auyón <diegoeauyonh@gmail.com>
2021-03-04 00:39:44 -0600

    Remove all *.pyc

--- proposals_categories.py
+++ proposals_categories.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 
 _TOPIC_CHOICES = (('', _(u'Selecciona una categoría')),
     ('seguridad_alimenticia_y_nutricional', _(u'Seguridad alimenticia y nutricional')),
-    ('infraestructura_economia_y_trabajo', _(u'InfraestructuraEconomia y trabajo')),
+    ('infraestructura_economia_y_trabajo', _(u'Infraestructura, Economia y trabajo')),
     ('administración_pública', _(u'Administración pública')),
     ('derechos_sexuales_y_reproductivos', _(u'Derechos sexuales y reproductivos')),
     ('seguridad_y_justicia', _(u'Seguridad y justicia')),
@@ -12,4 +12,17 @@ _TOPIC_CHOICES = (('', _(u'Selecciona una categoría')),
     ('medio_ambiente_y_recursos_naturales', _(u'Medio ambiente  y recursos naturales')),
     ('cultura_y_deportes', _(u'Cultura y deportes')),
     ('lucha_contra_la_corrupción', _(u'Lucha contra la corrupción')),
+)
+
+_TOPIC_DESCRIPTIONS = (
+    ('seguridad_alimenticia_y_nutricional', _(u'Seguridad alimenticia y nutricional lorem ipsum a great description here please')),
+    ('infraestructura_economia_y_trabajo', _(u'Infraestructura, Economia y trabajo lorem ipsum a great description here please')),
+    ('administración_pública', _(u'Administración pública lorem ipsum a great description here please')),
+    ('derechos_sexuales_y_reproductivos', _(u'Derechos sexuales y reproductivos lorem ipsum a great description here please')),
+    ('seguridad_y_justicia', _(u'Seguridad y justicia lorem ipsum a great description here please')),
+    ('transporte_público', _(u'Transporte público lorem ipsum a great description here please')),
+    ('salud', _(u'Salud lorem ipsum a great description here please')),
+    ('medio_ambiente_y_recursos_naturales', _(u'Medio ambiente  y recursos naturales lorem ipsum a great description here please')),
+    ('cultura_y_deportes', _(u'Cultura y deportes lorem ipsum a great description here please')),
+    ('lucha_contra_la_corrupción', _(u'Lucha contra la corrupción lorem ipsum a great description here please')),
 )
